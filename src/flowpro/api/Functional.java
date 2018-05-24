ecc6e7b84cd3d20d5d859b2b74822aa339ecdfc8
„obublik“ <obublik@ntis.zcu.cz>
2018-05-24 09:48:55 +0200

    change functional

--- src/flowpro/api/Functional.java
+++ src/flowpro/api/Functional.java
@@ -8,11 +8,14 @@ import java.io.*;
  */
 
 public interface Functional extends Serializable {
+    
+    public int getN();
 
     public void init(FlowProProperties props) throws IOException;
     
-    public double insideValue(double[] W, double[] dW, ElementData elemData);
+    public double[] insideValue(double[] W, double[] dW, ElementData elemData);
     
-    public double boundaryValue(double[] W, double[] dW, double[] n, int TT, ElementData elemData);
+    public double[] boundaryValue(double[] W, double[] dW, double[] n, int TT, ElementData elemData);
     
+    public double combineFunctionals(double[] functionals);   
 }
